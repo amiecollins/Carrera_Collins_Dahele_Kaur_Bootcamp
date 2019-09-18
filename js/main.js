9b1ff4178879bb641082519ae5b921b657e2516c
Walter Sebastian Carrera <cwalter1994@gmail.com>
2019-09-18 16:52:38 -0400

    about page created

--- js/main.js
+++ js/main.js
@@ -25,3 +25,79 @@ function slideshow () {
 
     setTimeout(slideshow, 10000);
 }
+
+//Lightbox functionality
+(() => {
+
+	console.log("javascript enabled");
+	
+	const lightBox = document.querySelectorAll(".light-box");
+	const exitButtons = document.querySelectorAll(".exit-button");
+	
+    const play = document.querySelector(".play-button");
+	const pause = document.querySelector(".pause-button");
+	const stop = document.querySelector(".stop-button");
+	const rewind = document.querySelector(".rewind-button");
+	const fastForward = document.querySelector(".fast-forward-button");
+	const volumeUp = document.querySelector(".volume-up-button");
+    const volumeDown = document.querySelector(".volume-down-button");
+	const videoPlayer = document.querySelector(".video-player");
+
+
+	function showLightBox() {
+		var targetLightBox = lightBox[this.dataset.offset];
+		targetLightBox.classList.add('show-box');
+	}
+
+	function hideLightBox() {
+		for (i = 0; i < lightBox.length; i++) {
+			lightBox[i].classList.remove('show-box');
+			stopPressed();
+		}
+	}
+	
+	function playPressed() {
+		  videoPlayer.play();
+		  videoPlayer.volume(0.5);
+	}
+
+	function pausePressed() {
+		videoPlayer.pause(true);
+	}
+
+	function stopPressed() {
+		 videoPlayer.pause(true);
+		 videoPlayer.currentTime = 0;
+	}
+
+	function rewindPressed() {
+		videoPlayer.currentTime = currentTime - 5;
+	}
+
+	function fastForwardPressed() {
+		videoPlayer.currentTime = currentTime + 5;
+	}
+
+	function volumeUpPressed() {
+		if (videoPlayer.volume() < 1) {
+			videoPlayer.volume(videoPlayer.volume() + 0.1);
+		}
+	}
+	
+	function volumeDownPressed() {
+		if (videoPlayer.volume() > 0) {
+			videoPlayer.volume(videoPlayer.volume() - 0.1);
+		}
+    }
+    lightBox.forEach(lightbox => lightbox.addEventListener("click", showLightBox));
+	exitButtons.forEach(exitButton => exitButton.addEventListener("click", hideLightBox));
+
+	play.addEventListener("click", playPressed);
+	pause.addEventListener("click", pausePressed);
+	stop.addEventListener("click", stopPressed);
+	rewind.addEventListener("click", rewindPressed);
+	fastforward.addEventListener("click", fastForwardPressed);
+	volumeUp.addEventListener("click", volumeUpPressed);
+	volumeDown.addEventListener("click", volumeDownPressed);
+	
+})();
