5a03980d7517127945d8cbaa6a40e5b7d02e66c6
amiecollins <a_collins84448@fanshaweonline.ca>
2019-09-19 16:42:39 -0400

    lightbox working

---
+++ js/modules/lightboxData.js
@@ -0,0 +1,55 @@
+export default {
+    Walter: {
+        name: "Walter Carrera",
+        role: "Developer",
+        photourl: "../../images/walter.jpg",
+        description: "Bio Goes Here"
+    },
+
+    Harwinder: {
+        name: "",
+        role: "",
+        photourl: "../../images/.jpg",
+        description: "Bio Goes Here"
+    },
+
+    Amie: {
+        name: "",
+        role: "",
+        photourl: "../../images/.jpg",
+        description: "Bio Goes Here"
+    },
+
+    Prabh: {
+        name: "",
+        role: "",
+        photourl: "../../images/.jpg",
+        description: "Bio Goes Here"
+
+    },
+
+    Gill: {
+        name: "",
+        role: "",
+        photourl: "../../images/.jpg",
+        description: "Bio Goes Here"
+
+    },
+
+    Video: {
+        name: "We Are WHAP!",
+        role: "A Short Video About Us",
+        photourl: "",
+        description: "This is a short video made by our motion designers describing what we offer as a company."
+    },
+
+    TextATaxi: {
+        name: "Text-a-Taxi",
+        role: "Local Taxi Catalogue and Review",
+        photourl: "../../images/textataxiLightbox.jpg",
+        description: "Text-A-Taxi started as a passion project for our client and gained users quickly, the app was originally servicing only large cities in Ontario but with the help of us they have been able to provide taxi information for all of Canada. We revamped their logo as well as their app, adding new features such as auto-calling and auto-texting within the app and a favorite taxi option to save your favorite local taxis."
+    }
+
+
+
+}
