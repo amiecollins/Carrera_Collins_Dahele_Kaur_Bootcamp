304d462734255a6cc8f288525d04cc128b895d46
amiecollins <a_collins84448@fanshaweonline.ca>
2019-09-19 17:16:57 -0400

    bio lightboxes complete

--- js/modules/lightboxData.js
+++ js/modules/lightboxData.js
@@ -7,47 +7,45 @@ export default {
     },
 
     Harwinder: {
-        name: "",
-        role: "",
-        photourl: "../../images/.jpg",
+        name: "Harwinder Dahele",
+        role: "Motion Designer",
+        photourl: "../../images/harwinder_2.jpg",
         description: "Bio Goes Here"
     },
 
     Amie: {
-        name: "",
-        role: "",
-        photourl: "../../images/.jpg",
-        description: "Bio Goes Here"
+        name: "Amie Collins",
+        role: "Project Manager",
+        photourl: "../../images/Amie_2.jpg",
+        description: `Amie is a down-to-earth, easy-going person with a passion for creating. Believing everyone on a project should have a say when it comes to design and development, she works to combine the voices of client and team members into cohesive experiences that are relatable and comfortable to the target audience. With experience in front-end, graphic and motion design her determination to work with everybody peacefully is what brings happy clients and a happy team.`
     },
 
     Prabh: {
-        name: "",
-        role: "",
-        photourl: "../../images/.jpg",
-        description: "Bio Goes Here"
-
+        name: "Prabhjot -Prabh- Kaur",
+        role: "Graphic Designer",
+        photourl: "../../images/prabh_2.jpg",
+        description: `Bio Goes Here`
     },
 
     Gill: {
-        name: "",
-        role: "",
-        photourl: "../../images/.jpg",
-        description: "Bio Goes Here"
-
+        name: "Prabhjot -Gill- Kaur",
+        role: "Motion Designer",
+        photourl: "../../images/prabh_gill_2.jpg",
+        description: `Bio Goes Here`
     },
 
     Video: {
         name: "We Are WHAP!",
         role: "A Short Video About Us",
         photourl: "",
-        description: "This is a short video made by our motion designers describing what we offer as a company."
+        description: `This is a short video made by our motion designers describing what we offer as a company.`
     },
 
     TextATaxi: {
         name: "Text-a-Taxi",
         role: "Local Taxi Catalogue and Review",
         photourl: "../../images/textataxiLightbox.jpg",
-        description: "Text-A-Taxi started as a passion project for our client and gained users quickly, the app was originally servicing only large cities in Ontario but with the help of us they have been able to provide taxi information for all of Canada. We revamped their logo as well as their app, adding new features such as auto-calling and auto-texting within the app and a favorite taxi option to save your favorite local taxis."
+        description: `Text-A-Taxi started as a passion project for our client and gained users quickly, the app was originally servicing only large cities in Ontario but with the help of us they have been able to provide taxi information for all of Canada. We revamped their logo as well as their app, adding new features such as auto-calling and auto-texting within the app and a favorite taxi option to save your favorite local taxis.`
     }
 
 
